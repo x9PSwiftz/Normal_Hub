39d1ae2de0a799a1ff481b00d25686e609fcefc5
normalhubv1 <108181477+normalhubv1@users.noreply.github.com>
2022-06-25 19:28:20 +0700

    Update V1.lua

--- V1.lua
+++ V1.lua
@@ -1,1 +1,7 @@
-print("Normal_Hubv1")
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/BloxFruit.lua"), true))();
+elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/KingLegacy.lua "), true))();
+else
+    game.Players.LocalPlayer:Kick("Not Support Game")
+end
